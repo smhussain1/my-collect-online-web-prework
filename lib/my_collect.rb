3953e994bd20201b152d2ba599d7e721b511feb3
michael hussain <smhussain_1@yahoo.com>
2019-01-02 15:15:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
-
+def collect (arg)
+  array = []
+  counter = 0 
+  while counter < 10 
+    array.push puts "This is is the #{count} and this is the #{arg}."
+    end 
+    yield(array)
+  
+end 
