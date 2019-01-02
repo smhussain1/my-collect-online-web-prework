70829b19120febf0bcfd2d240d6f6e56871b4302
michael hussain <smhussain_1@yahoo.com>
2019-01-02 17:22:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,15 +2,11 @@
 def my_collect (arg)
   array = []
   counter = 0 
-  if arg.length == 0
-    y = "No Arguments Here!"
-    arg.push(y)
-  end
+ 
   while counter < arg.length 
-    
-    array.push(x)
+    array << yield(array[counter])
     counter += 1 
     end 
-  yield(array)
+  
 end 
 # x = puts "This is is the #{counter} and this is the #{arg}."
